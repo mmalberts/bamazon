5faa8af1d6d091248ffecc4fd88b7466a9613a22
mmalberts <maggiemalberts@gmail.com>
2018-02-28 22:25:57 -0600

    file name change and instructions

---
+++ bamazonCustomer.js
@@ -0,0 +1,13 @@
+
+// * * * B U I L D I N G  C U S T O M E R  I N T E R F A C E * * *
+
+// running this app will first display all the items available
+	// this will include ids, names, and prices for each
+// then prompt the user with two messages
+	// first message asks the id of the product they would like to buy
+	// second message asks how many of this item they would like to buy
+// once the customer has placed the order, app should check if the store has enough of the product in stock to fill the order.
+	// if not, the app should alert the customer and stop the order
+	// if there is enough, fulfill the order
+		// this means updating SQL database to reflect quality
+		// once the order goes through, show customer the total cost of their purchase
---
+++ bamazonSchema.sql
@@ -0,0 +1,13 @@
+
+-- * * * B U I L D I N G  D A T A B A S E * * *
+
+-- create database called bamazon
+-- create table in database called products
+-- products should have the following
+	-- item_id (unique for each product) - int auto increment
+	-- product_name (name of product (duh)) - varchar
+	-- department_name - varchar
+	-- price - decimal
+	-- stock_quantity (how many of each is available in store) - int
+-- populate database with 10 different products
+
