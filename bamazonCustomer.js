806fc7dab601d1331a08bf4bd32faf3b7f682633
mmalberts <maggiemalberts@gmail.com>
2018-03-01 10:15:09 -0600

    customer application working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,5 +9,114 @@
 // once the customer has placed the order, app should check if the store has enough of the product in stock to fill the order.
 	// if not, the app should alert the customer and stop the order
 	// if there is enough, fulfill the order
-		// this means updating SQL database to reflect quality
+		// this means updating SQL database to reflect quantity
 		// once the order goes through, show customer the total cost of their purchase
+
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+//making connection to database
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "",
+	database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+	if (err) throw err;
+	// console.log("Connected to bamazon_db"); //for debugging
+	displayItems();
+});
+
+//function to display all available items
+function displayItems() {
+	console.log("\n***WELCOME TO BAMAZON***\n");
+	console.log("\nItems available: \n");
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		// console.log(res);
+		for (var i = 0; i < res.length; i++) {
+			var row = res[i];
+			console.log("\n----------\n");
+			console.log("Item ID: " + row.item_id);
+			console.log("Product Name: " + row.product_name);
+			console.log("Price: $" + row.price);
+		};
+		promptCustomer();
+	});
+};
+
+var chosenId;
+var chosenQuant;
+
+
+//function that uses inquirer to prompt customer to purchase items
+function promptCustomer() {
+	inquirer.prompt([
+	{
+		name: "id",
+		type: "input",
+		message: "\nPlease input the ID number of the item you would like to purchase"
+	}, {
+		name: "quantity",
+		type: "input",
+		message: "\nHow many of this item would you like to purchase?"
+	}
+	]).then(function(response) {
+		chosenId = parseFloat(response.id);
+		chosenQuant = parseFloat(response.quantity);
+		var chosenItem;
+		connection.query("SELECT * FROM products", function(err, res) {
+			if (err) throw err;
+			for (var i = 0; i < res.length; i++) {
+				if (res[i].item_id === chosenId) {
+					chosenItem = res[i];
+				};
+			};
+			// console.log(chosenItem.product_name); //for debugging
+			if (chosenItem.stock_quantity < chosenQuant) {
+				console.log("\nWe do not have enough of this item in stock.\n");
+				connection.end();
+			}
+			else {
+				var totalCost = chosenItem.price * chosenQuant;
+				var newStockQuant = chosenItem.stock_quantity - chosenQuant;
+				// console.log(newStockQuant);
+				// console.log(chosenItem.stock_quantity);
+				// console.log(chosenId);
+				console.log("\nThank you for your order. Your total is $" + totalCost +"\n");
+				//function that updates inventory quantity
+				connection.query(
+					"UPDATE products SET ? WHERE ?",
+					[
+						{
+							stock_quantity: newStockQuant
+						},
+						{
+							item_id: chosenId
+						}
+					],
+					function(err, res) {
+						if (err) throw err;
+						// console.log("Quantity updated");
+					}
+				);
+			};
+			// quantityCheck();
+			connection.end();
+		});
+	});
+}; 
+
+//quantity check function for debugging
+function quantityCheck() {
+	connection.query("SELECT * FROM products", function(err, res) {
+		if (err) throw err;
+		for (var i = 0; i < res.length; i++) {
+			console.log(res[i].stock_quantity);
+		}
+	})
+};
+
--- bamazonSchema.sql
+++ bamazonSchema.sql
@@ -11,3 +11,29 @@
 	-- stock_quantity (how many of each is available in store) - int
 -- populate database with 10 different products
 
+drop database if exists bamazon_db;
+create database bamazon_db;
+
+use bamazon_db;
+
+create table products (
+	item_id int not null auto_increment,
+	product_name varchar(50) not null,
+	department_name varchar(50) not null,
+	price decimal(10,2) not null,
+	stock_quantity int not null,
+	primary key (item_id)
+);
+
+insert into products (product_name, department_name, price, stock_quantity)
+	values 
+	("bamboo toothbrush", "health and beauty", 2.00, 500), 
+	("hammock", "outdoor gear", 40.00, 200), 
+	("winter hat", "clothing", 25.00, 300), 
+	("bench", "furniture", 430.00, 50), 
+	("dog food", "pets", 15.00, 200), 
+	("vitamins", "health and beauty", 8.00, 400), 
+	("striped socks", "clothing", 3.00, 500), 
+	("camping tent", "outdoor gear", 75.00, 200), 
+	("warm blanket", "home", 30.00, 300), 
+	("hiking boots", "clothing", 50.00, 300);
